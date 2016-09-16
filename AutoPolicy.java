e62264619cc880702b56c71d4b9b2abb8bb66b94
pedroromn <pedroromn@gmail.com>
2016-09-16 13:06:36 -0500

    16/09/2016 13:06

--- AutoPolicy.java
+++ AutoPolicy.java
@@ -2,17 +2,63 @@
 // Class that represents an auto insurance policy
 
 public class AutoPolicy{
-    private String accountNumber; // policy account number
+    private int accountNumber; // policy account number
     private String makeAndModel; // car that the policy applies to
     private String state; // two-letter state abbreviation
 
     // constructor
-    public AutoPolicy(String accountNumber,
+    public AutoPolicy(int accountNumber,
                       String makeAndModel,
                       String state){
         this.accountNumber = accountNumber;
         this.makeAndModel = makeAndModel;
         this.state = state;
+    }
+
+    // set the accountNumber
+    public void setAccountNumber(int accountNumber){
+        this.accountNumber = accountNumber;
+    }
+
+    // return the accountNumber
+    public int getAccountNumber(){
+        return this.accountNumber;
+    }
+
+    public void setMakeAndModel(String makeAndModel){
+        this.makeAndModel = makeAndModel;
+    }
+
+    public String getMakeAndModel(){
+        return this.makeAndModel;
+    }
+
+    public void setState(String state){
+        this.state = state;
+    }
+
+    public String getState(){
+        return this.state;
+    }
+
+
+    // predicate method returns whether the state has no-fault insurance
+    public boolean isNoFaultState(){
+
+        boolean noFaultState;
+
+        // determine whether the state has no-fault auto insurance
+        switch(this.getState()){
+
+        case "MA": case "NJ": case "NY": case "PA":
+            noFaultState = true;
+            break;
+        default:
+            noFaultState = false;
+            break;
+        }
+
+        return noFaultState;
     }
 
     
